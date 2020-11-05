01ff0e1362d8651bee5c988359666107dfec999e
ixphinx <48696004+ixphinx@users.noreply.github.com>
2020-11-05 17:03:36 -0300

    asd

--- src/models/database.js
+++ src/models/database.js
@@ -2,30 +2,8 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const Data = new Schema({
-   /* Usuarios */
-    user: String,
-    password: String,
-    username: String,
-    userlastname: String,
-    usercountry: String,
-    userteam: String,
-    useralias: String,
-    userscore: Number,
-    userexp: Number,
-    newuser: Boolean,
-   userbrzcup: Number,
-   usersilvcup: Number,
-   usergoldcup: Number,
-    
-    /* Trivia */
-    main: String,
-    type: String,
-    code: Number,
-    pregunta: String,
-    ranswer: String,
-    answer: String,
-    answerid: String,
-    rate: Number,
+   linkpais: String,
+   link: String,
     
     
 });
